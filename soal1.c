55aaf9a0f41e2bb71e2bcbf17b2e9fc806fd953e
Bismar404 <alwibismar@gmail.com>
2026-04-15 10:01:39 +0700

    Implement stair descent calculation with broken steps
    This program calculates the number of ways to descend stairs while avoiding broken steps. It takes the total number of stairs and the broken steps as input.

--- soal1.c
+++ soal1.c
@@ -1,5 +1,54 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 01 - Overview of C Language
+ *   Hari dan Tanggal    : Rabu, 15 April 2026
+ *   Nama (NIM)          : Bismar Alwi Khadavi (13224016)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Menghitung jumlah cara untuk turun tangga dengan melewati tangga yang rusak dan sekali turun bisa 1 atau 2 tangga
+ *                         input: N (jumlah tangga), R (jumlah tangga rusak), dan nomor tangga yang rusak
+ *                         output: jumlah cara untuk turun tangga
+ */
 
-int main(void) {
+
+#include <stdio.h>
+#include <string.h>
+
+int N, R;
+int rusak[1000];
+
+int hitungCara(int current){
+    //kalau hanya 1 tangga, hanya ada 1 caranya
+    if(current == 0) {
+        return 1;
+    }
+    //kalau tangga yang diinjak rusak atau tangga tidak valid, tidak ada cara turun
+    if(current<0 || rusak[current] == 1){
+        return 0;
+    }
+    
+    int cara = 0;
+    
+    //prioritas coba turun 1 tangga
+    if(current-1 >= 0 && rusak[current-1] == 0) {
+        cara += hitungCara(current-1);
+    }
+    
+    //kalau bisa turun 2 tangga, coba cara turun 2 tangga
+    if(current-2 >= 0 && rusak[current-2] == 0) {
+        cara += hitungCara(current-2);
+    }
+    return cara;
+}
+
+int main() {
+    scanf("%d %d", &N, &R);
+    for (int i = 0; i < R; i++) {
+        int rusakk;
+        scanf("%d", &rusakk);
+        rusak[rusakk] = 1;
+    }
+    
+    int cara = hitungCara(N);
+    printf("%d\n", cara);
     return 0;
 }
+ 
